64b385d25e2cccf378d62f38e31f7bdf20cf0e0d
mdbassoon <mark.delgado87@gmail.com>
2024-03-29 15:49:36 -0700

    qa 3

--- themes/hngrooming/template-parts/content.php
+++ themes/hngrooming/template-parts/content.php
@@ -57,7 +57,4 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php hngrooming_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
